f01c3deef5b51a1d94b9a0a7dea1b01aa6c200a7
sak345 <59903178+sak345@users.noreply.github.com>
2023-07-30 02:14:59 +0530

    Add files via upload

--- models/campgrounds.js
+++ models/campgrounds.js
@@ -2,11 +2,32 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const campgroundSchema = new Schema({
-    title: String,
-    price: Number,
-    img: String,
-    description: String,
-    location: String
+    title: {
+        type: String,
+        required: [true, "Name cannot be empty."]
+    },
+    price: {
+        type: Number,
+        required: [true, "Price cannot be empty."]
+    },
+    img: {
+        type: String,
+        required: [true, "Image cannot be empty."]
+    },
+    description: {
+        type: String,
+        required: [true, "Description cannot be empty."]
+    },
+    location: {
+        type: String,
+        required: [true, "Location cannot be empty."]
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 })
 
 module.exports = mongoose.model('Campground', campgroundSchema)
